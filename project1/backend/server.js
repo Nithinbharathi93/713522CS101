56e84bc10ba3fa8ef1d934ccdde2e853aec3b424
Nithinbharathi93 <nithinthelordest@gmail.com>
2025-03-28 13:47:56 +0530

    server running perfectly

--- project1/backend/server.js
+++ project1/backend/server.js
@@ -1,3 +1,14 @@
 import express from 'express';
-import axios from 'axois';
+// import axios from 'axois';
+import dotenv from 'dotenv';
+import cors from 'cors';
 
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Server running ar http://localhost:${PORT}`))
